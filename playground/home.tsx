e09af8269db8addc3bea34a7184656240d9dd8c8
Anh (Daniel) Le <ledzanh@gmail.com>
2023-10-25 09:04:00 +0000

    feat: init playground

---
+++ playground/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div className="home">
+      <h1>Guides</h1>
+      <p>This is the testing area for components during dev process.</p>
+      <p>Each component must be in a separated page.</p>
+      <p>
+        Just add new file inside <span>components</span> and update the <span>ROUTES</span> value in{" "}
+        <span>main.tsx</span> file
+      </p>
+    </div>
+  );
+}
---
+++ playground/main.tsx
@@ -0,0 +1,44 @@
+import React, { Suspense, lazy } from "react";
+import { render } from "react-dom";
+import { JsxElement } from "typescript";
+import { Link, Route, useLocation } from "wouter";
+
+const ROUTES = [
+  { label: "Home", route: "/", component: lazy(() => import("./home")) },
+  { label: "Button", route: "/button", component: lazy(() => import("./components/button")) },
+];
+
+function App() {
+  const [location] = useLocation();
+
+  return (
+    <>
+      <nav className="nav-bar">
+        {ROUTES.map((r, index) => (
+          <Link href={r.route} key={index}>
+            <span className={location === r.route ? "active" : ""}>{r.label}</span>
+          </Link>
+        ))}
+      </nav>
+
+      <br />
+
+      {ROUTES.map((r, index) => {
+        // biome-ignore lint: workaround to bypass ts errors
+        const Component = r.component as any;
+        // biome-ignore lint: workaround to bypass ts errors
+        const S = Suspense as any;
+
+        return (
+          <Route key={index} path={r.route}>
+            <S fallback={<div>loading...</div>}>
+              <Component />
+            </S>
+          </Route>
+        );
+      })}
+    </>
+  );
+}
+
+render(<App />, document.querySelector("#root"));
