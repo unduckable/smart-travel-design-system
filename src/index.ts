bedad4113ce58f8107b612ab7774a1eb52a1ac9f
Hieu Do <hieudo1@beeketing.net>
2023-10-31 21:07:13 +0700

    feat: add Checkbox component

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import "./styles/index.css";
 
 export { Button } from "./components/button";
 export type { IButton } from "./components/button";
+export { Checkbox } from "./components/checkbox";
+export type { ICheckbox } from "./components/checkbox";
