63cf36a337907e7e22bdaffc803c13a479bb4e5d
Hieu Do <hieudo1@beeketing.net>
2023-10-31 22:08:27 +0700

    feat: add Text Field component

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { Button } from "./components/button";
 export type { IButton } from "./components/button";
 export { Checkbox } from "./components/checkbox";
 export type { ICheckbox } from "./components/checkbox";
+export { TextField } from "./components/textfield";
+export type { ITextField } from "./components/textfield";
