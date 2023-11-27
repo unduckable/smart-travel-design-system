bab2b5cbd24916e9a56e02546c96bfbecb8a3b81
Anh Le <ledzanh@gmail.com>
2023-11-27 15:02:25 +0700

    feat: expose components to index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,18 @@ import "./styles/index.css";
 
 export { Button } from "./components/button";
 export type { IButton } from "./components/button";
+
 export { Checkbox } from "./components/checkbox";
 export type { ICheckbox } from "./components/checkbox";
-export { TextField } from "../_in_progress/textfield";
-export type { ITextField } from "../_in_progress/textfield";
+
+export { ButtonGroup } from "./components/button-group";
+export type { IButtonGroup } from "./components/button-group";
+
+export { Radio } from "./components/radio";
+export type { IRadio } from "./components/radio";
+
+export { RadioGroup } from "./components/radio-group";
+export type { IRadioGroup } from "./components/radio-group";
+
+export { Switch } from "./components/switch";
+export type { ISwitch } from "./components/switch";
