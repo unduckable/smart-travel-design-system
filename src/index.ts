2f7ac8814faf40cd3714ce6cc4096851f8a8ef33
Hieu Do <hieudo1@beeketing.net>
2023-12-06 21:28:15 +0700

    feat: export missing components

--- src/index.ts
+++ src/index.ts
@@ -47,3 +47,48 @@ export type { IDropdown, IDropdownItem } from "./components/dropdown";
 
 export { Select } from "./components/select";
 export type { ISelect } from "./components/select";
+
+export { Modal } from "./components/modal";
+export type { IModal } from "./components/modal";
+
+export { Dialog } from "./components/dialog";
+export type { IDialog } from "./components/dialog";
+
+export { Stepper } from "./components/stepper";
+export type { IStepper } from "./components/stepper";
+
+export { NavigationBar } from "./components/navigation-bar";
+export type { INavigationBar, INavItems } from "./components/navigation-bar";
+
+export { PageHeader } from "./components/page-header";
+export type { IPageHeader, IPageHeaderAction } from "./components/page-header";
+
+export { Carousel } from "./components/carousel";
+export type { ICarousel, ICarouselItem } from "./components/carousel";
+
+export { Badge } from "./components/badge";
+export type { IBadge } from "./components/badge";
+
+export { DatePicker } from "./components/date-picker";
+export type { IDatePicker } from "./components/date-picker";
+
+export { Slider } from "./components/slider";
+export type { ISlider } from "./components/slider";
+
+export { TextArea } from "./components/textarea";
+export type { ITextArea } from "./components/textarea";
+
+export { Tooltip } from "./components/tooltip";
+export type { ITooltip } from "./components/tooltip";
+
+export { Tab } from "./components/tab";
+export type { ITab } from "./components/tab";
+
+export { Tabs } from "./components/tabs";
+export type { ITabs } from "./components/tabs";
+
+export { TabPanel } from "./components/tab-panel";
+export type { ITabPanel } from "./components/tab-panel";
+
+export { TabList } from "./components/tab-list";
+export type { ITabList } from "./components/tab-list";
