559e8b293af49ece322e5b8669c8b2fcf5e980ff
Hieu Do <hieudo1@beeketing.net>
2023-12-20 23:47:37 +0700

    fix: avoid including CJS modules in build output

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,15 +1,12 @@
-import tailwindConfig from "@/tailwind.config.js";
-import resolveConfig from "tailwindcss/resolveConfig";
-
-const fullConfig = resolveConfig(tailwindConfig);
+import { BREAKPOINTS } from "../constants";
 
 export const getBreakpointValue = (value: string): number =>
-  +fullConfig.theme.screens[value].slice(0, fullConfig.theme.screens[value].indexOf("px"));
+  BREAKPOINTS[value].slice(0, BREAKPOINTS[value].indexOf("px"));
 
 export const getCurrentBreakpoint = (): string => {
   let currentBreakpoint: string;
   let biggestBreakpointValue = 0;
-  for (const breakpoint of Object.keys(fullConfig.theme.screens)) {
+  for (const breakpoint of Object.keys(BREAKPOINTS)) {
     const breakpointValue = getBreakpointValue(breakpoint);
     if (breakpointValue > biggestBreakpointValue && window.innerWidth >= breakpointValue) {
       biggestBreakpointValue = breakpointValue;
