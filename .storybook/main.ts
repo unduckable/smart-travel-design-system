bbf94af05103765414cebde4c2783203aae59496
Hieu Do <hieudo1@beeketing.net>
2023-12-24 13:42:55 +0700

    feat: add Dark mode & custom logo to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const propsBlackListWords = ["aria", "form"];
 const config: StorybookConfig = {
   stories: ["../src/storybook/welcome.mdx", "../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    "storybook-dark-mode",
     "@storybook/addon-links",
     {
       name: "@storybook/addon-essentials",
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"], // TODO: exclude storybook and test files
   safelist: [
     {
